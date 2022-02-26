1abeed81cd31b32dfb068b4f146996f7e73e94c7
justinZ45 <justinzaluk@gmail.com>
2022-02-26 18:12:00 -0500

    Added optional features

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,15 +12,68 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        //Styling options for the flashcard
+        //Round corners and shadow
+        card.layer.cornerRadius = 20.0;
+        card.layer.shadowRadius = 15.0;
+        card.layer.shadowOpacity = 0.5;
+        
+        backLabel.layer.cornerRadius = 20.0;
+        backLabel.clipsToBounds = true;
+        
+        frontLabel.layer.cornerRadius = 20.0;
+        frontLabel.clipsToBounds = true;
+        
+        btnOptionOne.layer.cornerRadius = 20.0;
+        btnOptionOne.clipsToBounds = true;
+        btnOptionOne.layer.borderWidth = 3.0;
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1);
 
+        btnOptionTwo.layer.cornerRadius = 20.0;
+        btnOptionTwo.clipsToBounds = true;
+        btnOptionTwo.layer.borderWidth = 3.0;
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1);
+        
+        btnOptionThree.layer.cornerRadius = 20.0;
+        btnOptionThree.clipsToBounds = true;
+        btnOptionThree.layer.borderWidth = 3.0;
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1);
+        
+        view.backgroundColor = #colorLiteral(red: 0.9704111218, green: 0.9603078961, blue: 0.9692524076, alpha: 1);
+        
+    }
+     
+      //hides & unhides card when tapped
     @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if(frontLabel.isHidden == true)
+        {
+        frontLabel.isHidden = false;
+        }
+        else{
+            frontLabel.isHidden = true;
+        }
+    }
+    
+    //button functions to check for correct answer when user taps them
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true;
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
         frontLabel.isHidden = true;
     }
     
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true;
+    }
 }
 
--- readme.md
+++ readme.md
@@ -3,9 +3,9 @@
 ## Lab 1
 
 ### App Description
-This Flashcards app presents the user with a specific question, presented in there form of a card. When the user taps on this flashcard, the answer to the question is revealed in place of it.
+This Flashcards app presents the user with a specific question, presented in the form of a card. When the user taps on this flashcard, the answer to the question is revealed in place of it. If it is tapped a second time, the question can be viewed again. Multiple choice answer buttons are placed beneath the flashcard, allowing the user to pick an option. If the answer is incorrect, the button will disappear. If the answer is correct, the answer will be revealed in place of the flashcard.
 
-<img src="YOUR_GIF_URL_HERE" width=200><br>
+<img src="file:///Users/justinzaluk/Desktop/CodePath%20iOS/Lab1/ezgif.com-gif-maker.gif" width=200><br>
 
 ## Required
 - [x] Create New Project in Xcode
@@ -14,6 +14,6 @@ This Flashcards app presents the user with a specific question, presented in the
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
